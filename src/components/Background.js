7bc46eb5b80a50602349c9306409c181efe40e08
Mateus <matbessa12@gmail.com>
2024-08-21 14:07:44 -0300

    Update Background.js and Background.css to adjust halo elements and use CSS variables
      - Replaced `backline-1` and `backline-2` classes with `yellow-halo` and `red-halo`
      - Introduced CSS variables for pixel values such as dimensions, positions, and colors
      - Applied these variables to the `background`, `yellow-halo`, and `red-halo` elements for easier adjustments

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,8 +3,8 @@ import '../css/Background.css';
 const Background = () => {
     return (
         <div className="background">
-            <div className="backline backline-1"></div>
-            <div className="backline backline-2"></div>
+            <div className="backline yellow-halo"></div>
+            <div className="backline red-halo"></div>
         </div>
     );
 }
